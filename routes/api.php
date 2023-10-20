329be698f0d2a3307d3074147a0b05c9aeb30574
herdypad <herdypad@gmail.com>
2023-10-20 15:45:05 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,19 @@ Route::post('register', [ApiAuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [ApiAuthController::class, 'logout']);
+
+    Route::post('clockin', [ApiPresensiController::class, 'absen']);
+
+    Route::post('updatefoto', [ApiUserController::class, 'ajukanFoto']);
+
+    Route::get('dataabsen/{id}', [ApiPresensiController::class, 'dataPresensiApi']);
 });
 
 
-Route::post('clockin', [ApiUserController::class, 'absen']);
-Route::post('updatefoto', [ApiUserController::class, 'ajukanFoto']);
 
 
-Route::get('dataabsen/{id}', [ApiPresensiController::class, 'dataPresensiApi']);
+
+
 
 
 
